f47492e994b6e9409dfeb6423a2767d4ca51d76f
Galuh Alifani <galuh.adika@gmail.com>
2021-08-04 03:09:16 +0700

    script

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const router = require("./routes/index.js");
 // const { autoAchievement } = require("./controllers/controllerAchievement");
 
 // var cron = require("node-cron");
 
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
