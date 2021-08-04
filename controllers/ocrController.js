94cf83b041a8f2c987238a015c38f8af108efed0
Galuh Alifani <galuh.adika@gmail.com>
2021-08-04 14:23:24 +0700

    constraints

--- controllers/ocrController.js
+++ controllers/ocrController.js
@@ -2,36 +2,43 @@ const tesseract = require("../OCR");
 
 class OCRController {
   static postOcr(req, res) {
-    const imageUrl = req.file.buffer;
+    console.log(req.file.size, 'SIZE')
 
-    console.log(imageUrl, 'IMAGEURL')
+    if (req.file.size > 350000) {
+      console.log('image is too large');
+      res.status(400).json({ message: "image is too large" });
+    } else {
+      const imageUrl = req.file.buffer;
 
-    // console.log("masuk controller", imageUrl);
-    // const imageUrl = req.urlImage
-    // const { imageUrl } = req.body
-    // console.log(req.body)
-    // console.log("gambar di ocrController, BUFFER", imageUrl);
-
-    if (!imageUrl) return res.status(400).json({ message: "bad request" });
-
-    tesseract(imageUrl)
-      .then((dataObj) => {
-        // console.log("masuk tesseract", dataObj);
-        if (dataObj) {
-          console.log('SUCCESS!')
-          res.status(200).json({ ...dataObj });
-        } else {
-          res.status(200).json({});
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({ message: "fail" });
-      });
-    // if (totalPrice) {
-    // } else {
-
-    // }
+      console.log(imageUrl, 'IMAGEURL')
+  
+      // console.log("masuk controller", imageUrl);
+      // const imageUrl = req.urlImage
+      // const { imageUrl } = req.body
+      // console.log(req.body)
+      // console.log("gambar di ocrController, BUFFER", imageUrl);
+  
+      if (!imageUrl) return res.status(400).json({ message: "bad request" });
+  
+      tesseract(imageUrl)
+        .then((dataObj) => {
+          // console.log("masuk tesseract", dataObj);
+          if (dataObj) {
+            console.log('SUCCESS!')
+            res.status(200).json({ ...dataObj });
+          } else {
+            res.status(200).json({});
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({ message: "fail" });
+        });
+      // if (totalPrice) {
+      // } else {
+  
+      // }  
+    }
   }
 }
 module.exports = OCRController;
